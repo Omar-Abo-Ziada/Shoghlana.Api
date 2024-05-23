ec70c2854ce67ef5273db96b674ab08eced75a7a
Saeed Mohamed <132412767+Saeed096@users.noreply.github.com>
2024-05-23 18:06:41 +0300

    .

--- Shoghlana.Core/Interfaces/IRepository.cs
+++ Shoghlana.Core/Interfaces/IRepository.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Shoghlana.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +10,41 @@ namespace Shoghlana.Core.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        public List<T> GetAll(string include = null);
-        public T GetById(int? Id);
-        public List<T> Get(Func<T, bool> where);
-        public void Insert(T item);
-        public void Update(T item);
-        public void Delete(T item);
-        public void Save();
+        public IEnumerable<T> GetAll();
+        public Task<IEnumerable<T>> GetAllAsync();
 
+        public T GetById(int id);
+        public Task<T> GetByIdAsync(int id);
 
+        public T Find(Expression<Func<T, bool>> criteria, string[] includes = null);
 
+        public Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null);
 
+        public IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, string[] includes = null);
 
+        public IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int skip, int take);
 
+        public IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int? skip, int? take,
+       Expression<Func<T, object>> orderBy = null, OrderWay orderByDirection = OrderWay.Ascending);
 
+        public Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[] includes = null);
+        public Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, int take, int skip);
+
+        public Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, int? take, int? skip,
+           Expression<Func<T, object>> orderBy = null, OrderWay orderByDirection = OrderWay.Ascending);
+        public T Add(T entity);
+        public Task<T> AddAsync(T entity);
+        public IEnumerable<T> AddRange(IEnumerable<T> entities);
+        public Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        public T Update(T entity);
+        public void Delete(T entity);
+        public void DeleteRange(IEnumerable<T> entities);
+        public void Attach(T entity);
+        public void AttachRange(IEnumerable<T> entities);
+        public int Count();
+        public int Count(Expression<Func<T, bool>> criteria);
+        public Task<int> CountAsync();
+        public Task<int> CountAsync(Expression<Func<T, bool>> criteria);
 
     }
 }
