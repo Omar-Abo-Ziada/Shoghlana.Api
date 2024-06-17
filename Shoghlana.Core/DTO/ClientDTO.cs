719c7577c2018e57cd8184311c1eda3bd233f923
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-06-17 17:21:54 +0300

    added Saeed Modifications on Client DTO

--- Shoghlana.Core/DTO/ClientDTO.cs
+++ Shoghlana.Core/DTO/ClientDTO.cs
@@ -11,16 +11,24 @@ namespace Shoghlana.Core.DTO
 {
     public class ClientDTO
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage = " Name Is Required")]
         [MinLength(3, ErrorMessage = "Name must be 3 char at least")]
         public string Name { get; set; }
 
         public string? Description { get; set; }
-        public IFormFile ? Image { get; set; }
 
-      
+        public IFormFile? Image { get; set; }
+
+        public DateTime RegisterationTime { get; set; }
+
         public string? Country { get; set; }
 
         public string? Phone { get; set; }
+
+        public int JobsCount { get; set; }
+
+        public int CompletedJobsCount { get; set; }
     }
 }
